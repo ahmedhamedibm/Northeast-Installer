c22ee61efee3c807418206ac7d66930eb738e119
Ahmed Hamed <ahamed@li-6a31b6cc-2e6d-11b2-a85c-904fbabf1421.ibm.com>
2022-09-05 21:07:46 -0400

    podman install

--- nei-helper.sh
+++ nei-helper.sh
@@ -3,5 +3,5 @@ cd ~/bin
 curl -sSL https://raw.github.ibm.com/National-Northeast-1/CLI_Installer/main/nei?token=AACTOHIDAVXUQRZQLH6P33DDD6336 >> nei
 chmod +x nei
 echo 'export PATH=$PATH":$HOME/bin"' >> ~/.bashrc
-. nei --installdep
+
 
