d29a4d1641b57e4f056b28052b3f19fea6b6e56f
Ahmed Hamed <ahamed@li-6a31b6cc-2e6d-11b2-a85c-904fbabf1421.ibm.com>
2022-09-05 21:14:40 -0400

    rewrite file

--- nei-helper.sh
+++ nei-helper.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/bin
 cd ~/bin
-curl -sSL https://raw.github.ibm.com/National-Northeast-1/CLI_Installer/main/nei?token=AACTOHPOY6LSCPYZHRT73HTDD7MNM >> nei
+curl -sSL https://raw.github.ibm.com/National-Northeast-1/CLI_Installer/main/nei?token=AACTOHPOY6LSCPYZHRT73HTDD7MNM > nei
 chmod +x nei
 echo 'export PATH=$PATH":$HOME/bin"' >> ~/.bashrc
 
