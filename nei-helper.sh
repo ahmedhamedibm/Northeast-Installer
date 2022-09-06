1f737d87f54896f7f2fc28ffa3e78644b7c599be
Ahmed Hamed <ahmedhamed@ibm.com>
2022-09-05 23:46:04 -0400

    Update nei-helper.sh

--- nei-helper.sh
+++ nei-helper.sh
@@ -1,7 +1,6 @@
 mkdir -p ~/bin
-cd ~/bin
-curl -sSL https://raw.github.ibm.com/National-Northeast-1/Northeast-Cli-Installer/main/nei?token=AACTOHMBVWCNWGKSNEQ7CW3DD75PA > nei
-chmod +x nei
+curl -sSL https://raw.github.ibm.com/National-Northeast-1/Northeast-Cli-Installer/main/nei?token=AACTOHK3KFJMNZMBVCYBMGLDD76MC > /bin/nei
+chmod +x /bin/nei
 echo 'export PATH=$PATH":$HOME/bin"' >> ~/.bashrc
 
 
